2f48a4aa4c0dadd3e7edca3e569fecc1114a3ec2
ArinaKaramova <karamowaarina@yandex.ru>
2021-10-04 12:55:12 +0500

    задание 1

---
+++ task1/task1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Вербочки");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Мальчики да девочки");
+            Console.WriteLine("Свечечки да вербочки");
+            Console.WriteLine("Понесли домой.");
+            Console.WriteLine("Огонечки теплятся,");
+            Console.WriteLine("Прохожие крестятся,");
+            Console.WriteLine("И пахнет весной.");
+            Console.ResetColor();
+
+               
+        }
+    }
+}

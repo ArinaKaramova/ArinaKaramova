51df19324ec538d55ba3f2952e7a18eee5f5b97a
ArinaKaramova <karamowaarina@yandex.ru>
2021-10-05 22:48:22 +0500

    задание2

---
+++ task2.1/task2.1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace task2._1
+{
+    class Program
+    {
+        static void Main()
+        {
+            var a = 6;
+            var b = 4;
+            
+            var e = Math.Pow(a, 2) + Math.Pow(b, 2); // квадрат катетов
+            
+            
+            var c = Math.Sqrt(e);// квадрат катетов
+
+
+            var m = c / 2;     
+            Console.WriteLine(m);
+            Console.ReadKey();
+
+
+
+
+
+                
+          
+           
+            
+            
+
+           
+           
+        }
+    }
+}
